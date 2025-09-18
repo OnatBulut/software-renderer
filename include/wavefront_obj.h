5c9efe17069eb9e81889d809db760f8ff174184d
Onat Bulut <96723086+OnatBulut@users.noreply.github.com>
2025-09-18 14:51:52 +0300

    Small fixes, added deliberate broken faces for test

--- include/wavefront_obj.h
+++ include/wavefront_obj.h
@@ -30,9 +30,12 @@ Mesh wavefront_obj_load_from_file(const char *filename, const unsigned int max_v
     vector_vertex normals = vector_vertex_create(NULL, 0);
     vector_vertex_reserve(&normals, 0x200);
 
+    unsigned int skipped = 0, line_idx = 0;
+
     char line[512];
     while (fgets(line, sizeof(line), file))
     {
+        ++line_idx;
         if (line[0] == 'v')
         {
             if(line[1] == ' ')
@@ -64,8 +67,8 @@ Mesh wavefront_obj_load_from_file(const char *filename, const unsigned int max_v
         }
         else if (line[0] == 'f')
         {
-            enum { POS = 1, TEX = 2, NORM = 4 } v_type = 0;
-            unsigned int v_cnt = 0, skipped = 0;
+            enum { UNK = 0, POS = 1, TEX = 2, NORM = 4 } v_type = UNK;
+            unsigned int v_cnt = 0;
             unsigned int p_in, p_val[max_v_per_face],
                          n_in, n_val[max_v_per_face],
                          t_in, t_val[max_v_per_face];
@@ -73,26 +76,32 @@ Mesh wavefront_obj_load_from_file(const char *filename, const unsigned int max_v
             char *t_line = strtok(line, " \t");
             while (t_line)
             {
+                if (v_cnt > max_v_per_face)
+                {
+                    printf("Face with %u or more vertices detected, consider increasing the limit.\n", max_v_per_face);
+                    break;
+                }
+
                 if (sscanf(t_line, "%u/%u/%u", &p_in, &t_in, &n_in) == 3)
                 {
                     p_val[v_cnt] = p_in;
                     t_val[v_cnt] = t_in;
                     n_val[v_cnt] = n_in;
-                    v_cnt++;
+                    ++v_cnt;
                     v_type = POS | TEX | NORM;
                 }
                 else if (sscanf(t_line, "%u//%u", &p_in, &n_in) == 2)
                 {
                     p_val[v_cnt] = p_in;
                     n_val[v_cnt] = n_in;
-                    v_cnt++;
+                    ++v_cnt;
                     v_type = POS | NORM;
                 }
                 else if (sscanf(t_line, "%u/%u", &p_in, &t_in) == 2)
                 {
                     p_val[v_cnt] = p_in;
                     t_val[v_cnt] = t_in;
-                    v_cnt++;
+                    ++v_cnt;
                     v_type = POS | TEX;
                 }
                 else if (sscanf(t_line, "%u", &p_in) == 1)
@@ -102,22 +111,17 @@ Mesh wavefront_obj_load_from_file(const char *filename, const unsigned int max_v
                 }
                 else if (strcmp(t_line, "f") != 0)
                 {
-                    fprintf(stderr, "Unrecognized face format!\n");
-                    skipped++;
-                    break;
-                }
-
-                if (v_cnt >= max_v_per_face)
-                {
-                    printf("Face with %u or more vertices detected, consider increasing the limit.\n", max_v_per_face);
+                    fprintf(stderr, "Unrecognized face format on line %u!\n", line_idx);
+                    v_type = UNK;
+                    ++skipped;
                     break;
                 }
 
                 t_line = strtok(NULL, " \t");
             }
 
-            if (!(v_type & POS))
-                return (Mesh) { 0 };
+            if (v_type == UNK)
+                continue;
 
             while (v_cnt > 2)
             {
@@ -125,6 +129,10 @@ Mesh wavefront_obj_load_from_file(const char *filename, const unsigned int max_v
                 glm_vec3_copy(vector_vertex_at(&positions, p_val[0] - 1)->p ,tri.v[0].p);
                 glm_vec3_copy(vector_vertex_at(&positions, p_val[v_cnt - 2] - 1)->p ,tri.v[1].p);
                 glm_vec3_copy(vector_vertex_at(&positions, p_val[v_cnt - 1] - 1)->p ,tri.v[2].p);
+                if (v_type & TEX)
+                {
+                    (void)t_val; // Will be added after textures are implemented
+                }
                 if (v_type & NORM)
                 {
                     glm_vec3_copy(vector_vertex_at(&normals, n_val[0] - 1)->n ,tri.v[0].n);
@@ -132,13 +140,13 @@ Mesh wavefront_obj_load_from_file(const char *filename, const unsigned int max_v
                     glm_vec3_copy(vector_vertex_at(&normals, n_val[v_cnt - 1] - 1)->n ,tri.v[2].n);
                 }
                 vector_triangle_push_back(&mesh.tris, tri);
-                v_cnt--;
+                --v_cnt;
             }
-
-            if (skipped > 0)
-                printf("Skipped %u faces.\n", skipped);
         }
     }
+
+    if (skipped > 0)
+        printf("Skipped %u faces.\n", skipped);
 
     vector_vertex_destroy(&positions);
     vector_vertex_destroy(&normals);
